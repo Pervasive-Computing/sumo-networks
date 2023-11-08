bb5bcab8e8107e3b8a0a67b83b0f88b98a6d7b0e
kpbaks <kristoffer.pbs@tuta.io>
2023-11-08 13:41:13 +0100

    feat: add parallel-hashmap and pugixml as conan dependencies

--- pretty-printers.hpp
+++ pretty-printers.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <string>
+#include <vector>
 #include <fmt/core.h>
 
 #include "ansi-escape-codes.hpp"
@@ -13,13 +14,32 @@ auto pprint_bool(bool b) -> void {
     fmt::print("{}{}{}\n", b ? escape_codes::color::fg::green : escape_codes::color::fg::red, b ? "true" : "false", escape_codes::reset);
 }
 
+auto pformat_string(const std::string& s) -> std::string {
+    return fmt::format("{}\"{}\"{}", escape_codes::color::fg::green, s, escape_codes::reset);
+}
+
+
+auto pformat(const std::string& s) -> std::string {
+    return pformat_string(s);
+}
+
+
+// template <typename T>
+// auto pprint(T&& t) -> void {}
+
+// template <>
+auto pprint(const std::string& s) -> void {
+    pprint_string(s);
+}
 
 template <typename T>
-auto pprint(T&& t) -> void {}
+auto pprint(const std::vector<T>& v, const int indent_by = 4, const int depth = 0) -> void {
 
-template <>
-auto pprint<const std::string&>(const std::string& s) -> void {
-    pprint_string(s);
+    fmt::print("{}{{{}\n", escape_codes::markup::bold, escape_codes::reset);
+    for (const auto& e : v) {
+        fmt::println("{}{},", std::string((depth + 1) * indent_by, ' '), pformat(e));
+    }
+    fmt::print("{}}}{}\n", escape_codes::markup::bold, escape_codes::reset);
 }
 
 // template <>
--- uwebsockets.cpp
+++ uwebsockets.cpp
@@ -1,16 +1,20 @@
+#include <chrono>
 #include <cmath>
+// #include <libsumo/Simulation.h>
+// #include <libsumo/Vehicle.h>
+using namespace std::chrono_literals;
+
 #include <filesystem>
+namespace fs = std::filesystem;
 #include <functional>
 #include <iostream>
 #include <optional>
 #include <string>
 #include <thread>
+#include <mutex>
 #include <vector>
 
-namespace fs = std::filesystem;
-
 #include "debug-macro.hpp"
-
 #include "pretty-printers.hpp"
 
 auto walkdir(
@@ -27,17 +31,22 @@ auto walkdir(
   return files;
 }
 
+// 3rd party libraries
 #include <argparse/argparse.hpp>
 #include <fmt/core.h>
-#include <uWebSockets/App.h>
-#include <spdlog/spdlog.h>
 #include <nlohmann/json.hpp>
+#include <spdlog/spdlog.h>
+#include <uWebSockets/App.h>
+#include <parallel_hashmap/phmap.h>
+#include <pugixml.hpp>
 
 // for convenience
 using json = nlohmann::json;
 using namespace nlohmann::literals; // for ""_json
 
+
 #include <libsumo/libtraci.h>
+using namespace libtraci;
 
 using u8 = std::uint8_t;
 using u16 = std::uint16_t;
@@ -60,23 +69,25 @@ template <typename T> using Option = std::optional<T>;
 struct Car {
   int x;
   int y;
+  double heading;
+  bool alive = false;
 };
 
 auto main(int argc, char **argv) -> int {
-    //   spdlog::info("Welcome to spdlog!");
-    // spdlog::error("Some error message with arg: {}", 1);
-    
-    // spdlog::warn("Easy padding in numbers like {:08d}", 12);
-    // spdlog::critical("Support for int: {0:d};  hex: {0:x};  oct: {0:o}; bin: {0:b}", 42);
-    // spdlog::info("Support for floats {:03.2f}", 1.23456);
-    // spdlog::info("Positional args are {1} {0}..", "too", "supported");
-    // spdlog::info("{:<30}", "left aligned");
-    
-    // spdlog::set_level(spdlog::level::debug); // Set global log level to debug
-    // spdlog::debug("This message should be displayed..");    
-    
-    // // change log pattern
-    // spdlog::set_pattern("[%H:%M:%S %z] [%n] [%^---%L---%$] [thread %t] %v");
+  //   spdlog::info("Welcome to spdlog!");
+  // spdlog::error("Some error message with arg: {}", 1);
+
+  // spdlog::warn("Easy padding in numbers like {:08d}", 12);
+  // spdlog::critical("Support for int: {0:d};  hex: {0:x};  oct: {0:o}; bin:
+  // {0:b}", 42); spdlog::info("Support for floats {:03.2f}", 1.23456);
+  // spdlog::info("Positional args are {1} {0}..", "too", "supported");
+  // spdlog::info("{:<30}", "left aligned");
+
+  // spdlog::set_level(spdlog::level::debug); // Set global log level to debug
+  // spdlog::debug("This message should be displayed..");
+
+  // // change log pattern
+  // spdlog::set_pattern("[%H:%M:%S %z] [%n] [%^---%L---%$] [thread %t] %v");
 
   // DEBUG_PRINTF("hello\n");
 
@@ -91,15 +102,23 @@ auto main(int argc, char **argv) -> int {
       .default_value(false)
       .implicit_value(true)
       .nargs(0);
+
   argument_parser.add_argument("-p", "--port")
       .default_value(9001)
       .scan<'i', int>()
       .help(fmt::format("0 < port <= {}", std::pow(2, 16) - 1));
+
   argument_parser.add_argument("--gui")
       .default_value(false)
       .implicit_value(true)
       .nargs(0)
-      .help("use `sumo-gui` instead of `sumo` to run the simulation");
+      .help("Use `sumo-gui` instead of `sumo` to run the simulation");
+
+  argument_parser.add_argument("--simulation-steps")
+      .default_value(10000)
+      .scan<'i', int>()
+      .help("Number of update steps to perform in the simulation");
+  // argument_parser
 
   argument_parser.add_argument("sumocfg").required().help(
       "SUMO configuration file");
@@ -114,36 +133,42 @@ auto main(int argc, char **argv) -> int {
 
   fs::path sumo_home_path;
   // Check that $SUMO_HOME is set
-  if (const char* sumo_home = std::getenv("SUMO_HOME")) {
-      // $SUMO_HOME is set
-      sumo_home_path = sumo_home;
-      // Check that $SUMO_HOME points to a directory that exists on the disk
-      if (!fs::exists(sumo_home_path)) {
-          spdlog::error("Environment variable $SUMO_HOME ({}) does not point to a valid directory", sumo_home);
-          return 2;
-      }
-      spdlog::info("SUMO_HOME: {}", sumo_home_path.string());
-  } else {
-      // $SUMO_HOME is not set
-      spdlog::error("Environment variable SUMO_HOME is not set");
+  if (const char *sumo_home = std::getenv("SUMO_HOME")) {
+    // $SUMO_HOME is set
+    sumo_home_path = sumo_home;
+    // Check that $SUMO_HOME points to a directory that exists on the disk
+    if (!fs::exists(sumo_home_path)) {
+      spdlog::error("Environment variable $SUMO_HOME ({}) does not point to a "
+                    "valid directory",
+                    sumo_home);
       return 2;
+    }
+    spdlog::info("SUMO_HOME: {}", sumo_home_path.string());
+  } else {
+    // $SUMO_HOME is not set
+    spdlog::error("Environment variable SUMO_HOME is not set");
+    return 2;
   }
-  
+
   const fs::path sumocfg_path = argument_parser.get<std::string>("sumocfg");
   if (!fs::exists(sumocfg_path)) {
     spdlog::error("SUMO configuration file not found: {}",
-                 sumocfg_path.filename().string());
-                 std::cerr << argument_parser;
+                  sumocfg_path.filename().string());
+    std::cerr << argument_parser;
     return 2;
   }
 
   if (!(sumocfg_path.has_extension()) ||
       sumocfg_path.extension().string() != ".sumocfg") {
-    spdlog::error("SUMO configuration file must have extension .sumocfg, not {}",
-                 sumocfg_path.extension().string());
-                 std::cerr << argument_parser;
+    spdlog::error(
+        "SUMO configuration file must have extension .sumocfg, not {}",
+        sumocfg_path.extension().string());
+    std::cerr << argument_parser;
     return 2;
   }
+
+  
+
 
   const fs::path cwd = fs::current_path();
 
@@ -152,40 +177,128 @@ auto main(int argc, char **argv) -> int {
     spdlog::debug("sumocfg: {}", sumocfg_path.string());
   }
 
-  if (sumocfg_path.has_parent_path()  && sumocfg_path.parent_path() != cwd) {
+  if (sumocfg_path.has_parent_path() && sumocfg_path.parent_path() != cwd) {
     // The SUMO configuration file is not in the current directory
-    // Change cwd to the parent directory of sumocfg, as 
+    // Change cwd to the parent directory of sumocfg, as
     // <net-file value="{...}.net.xml"/>
     // <route-files value="{...}.rou.xml"/>
     // <additional-files value="{...}.poly.xml"/>
-    // Most likely are defined relative to the parent directory of the sumocfg file.
+    // Most likely are defined relative to the parent directory of the sumocfg
+    // file.
     fs::current_path(sumocfg_path.parent_path());
     if (0 < verbosity) {
       fmt::println("cwd: {}", fs::current_path().string());
     }
   }
 
+  spdlog::info("cwd: {}", fs::current_path().string());
+
+  // Parse the SUMO configuration file
+  pugi::xml_document sumocfg_xml_doc;
+
+  pugi::xml_parse_result result = sumocfg_xml_doc.load_file(sumocfg_path.string().c_str());
+  if (!result) {
+    spdlog::error("Failed to parse SUMO configuration file: {}",
+                  result.description());
+    return 1;
+  }
+
+  const std::string route_files = sumocfg_xml_doc.child("configuration").child("input").child("route-files").attribute("value").as_string();
+  std::cerr << route_files << std::endl;
+
+  const fs::path route_files_path = fs::path(route_files);
+  if (!fs::exists(route_files_path)) {
+    spdlog::error("Route files not found: {}", route_files_path.string());
+    return 1;
+  }
+
+  pugi::xml_document route_files_xml_doc;
+
+  result = route_files_xml_doc.load_file(route_files_path.string().c_str());
+  if (!result) {
+    spdlog::error("Failed to parse route files: {}", result.description());
+    return 1;
+  }
+
+  phmap::flat_hash_map <std::string, Car> cars;
+
+  const auto vehicles = route_files_xml_doc.child("routes").children("vehicle");
+  for (const auto &vehicle : vehicles) {
+    const std::string vehicle_id = vehicle.attribute("id").as_string();
+    spdlog::info("vehicle: {}", vehicle_id);
+
+    cars[vehicle_id] = Car{};
+
+  }
+
+
+
+  // std::exit(0);
+
+
   // If sumocfg is not set in the command line, search for all files in
   // cwd/**/*.sumocfg and print them
   const int port = argument_parser.get<int>("port");
   if (port < 0 || port > std::pow(2, 16) - 1) {
-    fmt::println("Port must be between 0 and {}", std::pow(2, 16) - 1);
+    spdlog::error("Port must be between 0 and {}", std::pow(2, 16) - 1);
     return 1;
   }
 
-  auto sumo_simulation_thread = std::thread([&]() {
-    using namespace libtraci;
+  const i32 simulation_steps = argument_parser.get<int>("simulation-steps");
+  if (simulation_steps <= 0) {
+    spdlog::error("simulation-steps must be positive");
+    return 1;
+  }
+
+  spdlog::info("simulation-steps: {}", simulation_steps);
+  spdlog::info("port: {}", port);
+
+  auto sumo_simulation_thread = std::thread([&]() -> void {
     std::vector<std::string> cmd = {
         argument_parser.get<bool>("gui") ? "sumo-gui" : "sumo",
         "--configuration-file",
         sumocfg_path.string(),
     };
 
-    Simulation::start(cmd);
+    // const auto simulation_start_result = Simulation::start(cmd, 19000);
+    // spdlog::info(".first: {} .second: {}", simulation_start_result.first,
+    // simulation_start_result.second);
+
+    const int num_retries = 100;
+    const int sumo_sim_port = 10000;
+    Simulation::init(sumo_sim_port, num_retries, "localhost");
+    const double dt = Simulation::getDeltaT();
+
+    spdlog::info("dt: {}", dt);
 
     // TODO: figure out how to subscribe an extract data from the simulation
-    for (int i = 0; i < 10000; i++) {
+    for (int i = 0; i < simulation_steps; ++i) {
       Simulation::step();
+
+      // TODO: Get (x,y, theta) of all vehicles
+      auto vehicles_ids = Vehicle::getIDList();
+
+      for (const auto& id : vehicles_ids) {
+        const auto position = Vehicle::getPosition(id);
+        const int x = position.x;
+        const int y = position.y;
+
+        const auto heading = Vehicle::getAngle(id);
+
+        spdlog::info("id: {} x: {} y: {} heading: {}", id, x, y, heading);
+      }
+// static libsumo::TraCIPosition position;
+
+
+      // Vehicle::getPosition(const std::string &vehID)
+
+      // fmt::println("step: {}", i);
+      if (i % 100 == 0) {
+        // fmt::println("{} vehicles", vehicles_ids.size());
+        // pprint(vehicles_ids);
+      }
+
+      // std::this_thread::sleep_for(10ms);
     }
 
     Simulation::close();
