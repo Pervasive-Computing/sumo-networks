2eada973bd501cd7d159c90c204bdb7d5862de52
kpbaks <kristoffer.pbs@tuta.io>
2023-11-11 18:27:48 +0100

    refactor: put argv parsing into its own function

--- uwebsockets.cpp
+++ uwebsockets.cpp
@@ -4,7 +4,6 @@ using namespace std::chrono_literals;
 #include <filesystem>
 #include <queue>
 namespace fs = std::filesystem;
-// #include <expected>
 #include <functional>
 #include <iostream>
 #include <mutex>
@@ -12,22 +11,9 @@ namespace fs = std::filesystem;
 #include <string>
 #include <thread>
 #include <vector>
-
+#include "ansi-escape-codes.hpp"
 #include "debug-macro.hpp"
 #include "pretty-printers.hpp"
-
-auto walkdir(
-	const std::string& dir, const std::function<bool(const fs::directory_entry&)>& filter =
-								[](const auto& _) { return true; }) -> std::vector<std::string> {
-	std::vector<std::string> files;
-	for (const auto& entry : fs::directory_iterator(dir)) {
-		if (! filter(entry)) {
-			continue;
-		}
-		files.push_back(entry.path().string());
-	}
-	return files;
-}
 
 // 3rd party libraries
 #include <argparse/argparse.hpp>
@@ -65,6 +51,74 @@ using f64 = double;
 // template <typename T>
 // using Option = std::optional<T>;
 
+auto walkdir(
+	const std::string& dir, const std::function<bool(const fs::directory_entry&)>& filter =
+								[](const auto& _) { return true; }) -> std::vector<std::string> {
+	std::vector<std::string> files;
+	for (const auto& entry : fs::directory_iterator(dir)) {
+		if (! filter(entry)) {
+			continue;
+		}
+		files.push_back(entry.path().string());
+	}
+	return files;
+}
+
+// <configuration>
+//     <input>
+//         <net-file value="esbjerg.net.xml"/>
+//         <route-files value="esbjerg.rou.xml"/>
+//         <additional-files value="esbjerg.poly.xml"/>
+//     </input>
+//     <time>
+//         <begin value="0"/>
+//         <step-length value="0.001"/>
+//         <end value="20000"/>
+//     </time>
+//     <gui_only>
+//         <delay value="1"/>
+//         <start value="true"/>
+//     </gui_only>
+// </configuration>
+
+struct SumoConfiguration {
+	fs::path net_file;
+	fs::path route_files;
+	fs::path additional_files;
+};
+
+auto parse_sumocfg(const fs::path& sumocfg_path) -> tl::expected<SumoConfiguration, std::string> {
+	pugi::xml_document sumocfg_xml_doc;
+
+	pugi::xml_parse_result result = sumocfg_xml_doc.load_file(sumocfg_path.string().c_str());
+	if (! result) {
+		return tl::unexpected(fmt::format("Failed to parse SUMO configuration file: {}",
+										  result.description()));
+	}
+
+	const auto net_file = sumocfg_xml_doc.child("configuration")
+							  .child("input")
+							  .child("net-file")
+							  .attribute("value")
+							  .as_string();
+	const auto route_files = sumocfg_xml_doc.child("configuration")
+									.child("input")
+									.child("route-files")
+									.attribute("value")
+									.as_string();
+	const auto additional_files = sumocfg_xml_doc.child("configuration")
+									  .child("input")
+									  .child("additional-files")
+									  .attribute("value")
+									  .as_string();
+
+	return SumoConfiguration {
+		.net_file		 = net_file,
+		.route_files	 = route_files,
+		.additional_files = additional_files,
+	};
+}
+
 struct Car {
 	int	   x;
 	int	   y;
@@ -94,22 +148,54 @@ auto get_sumo_home_directory_path() -> tl::expected<fs::path, get_sumo_home_dire
 	}
 }
 
-auto main(int argc, char** argv) -> int {
-	int	 verbosity = 0;
+struct ProgramOptions {
+	u16 port;
+	u16 sumo_port;
+	i32  simulation_steps;
+	fs::path sumocfg_path;
+	bool gui = true;
+	bool verbose = false;
+};
+
+auto pprint(const ProgramOptions& options) -> void {
+	using namespace escape_codes;
+	fmt::println("({}ProgramOptions{}) {{", color::fg::cyan, reset);
+	const auto indent = std::string(4, ' ');
+	fmt::println("{}{}.port{} = {}{}{},", indent, markup::bold, reset, color::fg::blue, options.port, reset);
+	fmt::println("{}{}.sumo_port{} = {}{}{},", indent, markup::bold, reset, color::fg::blue, options.sumo_port, reset);
+	fmt::println("{}{}.simulation_steps{} = {}{}{},", indent, markup::bold, reset, color::fg::blue,
+				 options.simulation_steps, reset);
+	fmt::println("{}{}.sumocfg_path{} = {}{}{},", indent, markup::bold, reset, markup::italic,
+				 options.sumocfg_path.string(), reset);
+	fmt::println("{}{}.gui{} = {}{}{},", indent, markup::bold, reset, options.gui ? color::fg::green : color::fg::red, options.gui, reset);
+	fmt::println("{}{}.verbose{} = {}{}{},", indent, markup::bold, reset, options.verbose ? color::fg::green : color::fg::red,
+				 options.verbose, reset);
+	fmt::println("}};");
+}
+
+auto create_argv_parser() -> argparse::ArgumentParser {
 	auto argv_parser = argparse::ArgumentParser(__FILE__, "0.1.0");
 
 	argv_parser.add_argument("-V", "--verbose")
-		.action([&](const auto&) { ++verbosity; })
-		.append()
+		// .action([&](const auto&) { ++verbosity; })
+		// .append()
 		.default_value(false)
 		.implicit_value(true)
 		.nargs(0);
 
 	argv_parser.add_argument("-p", "--port")
-		.default_value(9001)
+		.default_value(12000)
 		.scan<'i', int>()
 		.help(fmt::format("Port used for the websocket server, constraints: 0 < port <= {}",
 						  std::pow(2, 16) - 1));
+
+						  argv_parser.add_argument("--sumo-port")
+		.default_value(12001)
+		.scan<'i', int>()
+		.help(fmt::format("Port used to connect to a running sumo simulation, constraints: 0 < port <= {}",
+						  std::pow(2, 16) - 1));
+
+						  
 
 	argv_parser.add_argument("--gui").default_value(false).implicit_value(true).nargs(0).help(
 		"Use `sumo-gui` instead of `sumo` to run the simulation");
@@ -121,14 +207,75 @@ auto main(int argc, char** argv) -> int {
 			  "simulation-steps");
 
 	argv_parser.add_argument("sumocfg").required().help("SUMO configuration file");
+	return argv_parser;
+}
 
+auto parse_args(argparse::ArgumentParser argv_parser, int argc, char** argv) -> tl::expected<ProgramOptions, std::string> {
 	try {
 		argv_parser.parse_args(argc, argv);
 	} catch (const std::exception& err) {
-		fmt::println("{}", err.what());
-		std::cerr << argv_parser;
-		return 2;
+		return tl::unexpected(fmt::format("{}", err.what()));
 	}
+
+	// If sumocfg is not set in the command line, search for all files in
+	// cwd/**/*.sumocfg and print them
+	const u16 port = argv_parser.get<int>("port");
+	if (port < 0 || port > std::pow(2, 16) - 1) {
+		return tl::unexpected(fmt::format("--port must be between 0 and {}", std::pow(2, 16) - 1));
+	}
+
+	const u16 sumo_port = argv_parser.get<int>("sumo-port");
+	if (sumo_port < 0 || sumo_port > std::pow(2, 16) - 1) {
+		return tl::unexpected(
+			fmt::format("--sumo-port must be between 0 and {}", std::pow(2, 16) - 1));
+	}
+
+	if (port == sumo_port) {
+		return tl::unexpected("--port and --sumo-port must be different");
+	}
+
+	const i32 simulation_steps = argv_parser.get<int>("simulation-steps");
+	if (simulation_steps <= 0) {
+		return tl::unexpected("simulation-steps must be positive");
+	}
+
+	// TODO: maybe convert to absolute path
+	const std::string sumocfg = argv_parser.get<std::string>("sumocfg");
+	const fs::path sumocfg_path = sumocfg;
+	if (! fs::exists(sumocfg_path)) {
+		return tl::unexpected(fmt::format("SUMO configuration file not found: {}",
+										  sumocfg_path.filename().string()));
+	}
+
+	if (! (sumocfg_path.has_extension()) || sumocfg_path.extension().string() != ".sumocfg") {
+		return tl::unexpected(fmt::format("SUMO configuration file must have extension .sumocfg, not {}",
+										  sumocfg_path.extension().string()));
+	}
+
+	return ProgramOptions {
+		.port			   = port,
+		.sumo_port		   = sumo_port,
+		.simulation_steps  = simulation_steps,
+		.sumocfg_path		   = sumocfg_path,
+		.gui			   = argv_parser.get<bool>("gui"),
+		.verbose		   = argv_parser.get<bool>("verbose"),
+	};
+}
+
+auto main(int argc, char** argv) -> int {
+	const auto argv_parser = create_argv_parser();
+	const auto print_help = [&]() -> void {
+		std::cerr << argv_parser;
+	};
+	const auto options = parse_args(argv_parser, argc, argv)
+							 .map_error([&](const auto& err) {
+								 spdlog::error("{}", err);
+								 print_help();
+								 std::exit(2);
+							 })
+							 .value();
+
+	pprint(options);
 
 	const auto sumo_home_path = [&]() {
 		auto result = get_sumo_home_directory_path();
@@ -148,29 +295,14 @@ auto main(int argc, char** argv) -> int {
 		}
 	}();
 
-
-	const fs::path sumocfg_path = argv_parser.get<std::string>("sumocfg");
-	if (! fs::exists(sumocfg_path)) {
-		spdlog::error("SUMO configuration file not found: {}", sumocfg_path.filename().string());
-		std::cerr << argv_parser;
-		return 2;
-	}
-
-	if (! (sumocfg_path.has_extension()) || sumocfg_path.extension().string() != ".sumocfg") {
-		spdlog::error("SUMO configuration file must have extension .sumocfg, not {}",
-					  sumocfg_path.extension().string());
-		std::cerr << argv_parser;
-		return 2;
-	}
-
 	const fs::path cwd = fs::current_path();
 
-	if (0 < verbosity) {
+	if (options.verbose) {
 		spdlog::debug("cwd: {}", cwd.string());
-		spdlog::debug("sumocfg: {}", sumocfg_path.string());
+		spdlog::debug("sumocfg: {}", options.sumocfg_path.string());
 	}
 
-	if (sumocfg_path.has_parent_path() && sumocfg_path.parent_path() != cwd) {
+	if (options.sumocfg_path.has_parent_path() && options.sumocfg_path.parent_path() != cwd) {
 		// The SUMO configuration file is not in the current directory
 		// Change cwd to the parent directory of sumocfg, as
 		// <net-file value="{...}.net.xml"/>
@@ -178,39 +310,21 @@ auto main(int argc, char** argv) -> int {
 		// <additional-files value="{...}.poly.xml"/>
 		// Most likely are defined relative to the parent directory of the sumocfg
 		// file.
-		fs::current_path(sumocfg_path.parent_path());
-		if (0 < verbosity) {
-			fmt::println("cwd: {}", fs::current_path().string());
-		}
+		fs::current_path(options.sumocfg_path.parent_path());
+		spdlog::warn("Changed cwd to {}", fs::current_path().string());
 	}
-
-	spdlog::info("cwd: {}", fs::current_path().string());
 
 	// Parse the SUMO configuration file
-	pugi::xml_document sumocfg_xml_doc;
-
-	pugi::xml_parse_result result = sumocfg_xml_doc.load_file(sumocfg_path.string().c_str());
-	if (! result) {
-		spdlog::error("Failed to parse SUMO configuration file: {}", result.description());
-		return 1;
-	}
-
-	const std::string route_files = sumocfg_xml_doc.child("configuration")
-										.child("input")
-										.child("route-files")
-										.attribute("value")
-										.as_string();
-	std::cerr << route_files << std::endl;
-
-	const fs::path route_files_path = fs::path(route_files);
-	if (! fs::exists(route_files_path)) {
-		spdlog::error("Route files not found: {}", route_files_path.string());
-		return 1;
-	}
+	const auto sumocfg = parse_sumocfg(options.sumocfg_path)
+							.map_error([](const auto& err) {
+								spdlog::error("{}", err);
+								std::exit(1);
+							})
+							.value();
 
 	pugi::xml_document route_files_xml_doc;
 
-	result = route_files_xml_doc.load_file(route_files_path.string().c_str());
+	const auto result = route_files_xml_doc.load_file(sumocfg.route_files.c_str());
 	if (! result) {
 		spdlog::error("Failed to parse route files: {}", result.description());
 		return 1;
@@ -222,50 +336,30 @@ auto main(int argc, char** argv) -> int {
 	const auto vehicles = route_files_xml_doc.child("routes").children("vehicle");
 	for (const auto& vehicle : vehicles) {
 		const std::string vehicle_id = vehicle.attribute("id").as_string();
-		spdlog::info("vehicle: {}", vehicle_id);
-
 		cars[vehicle_id] = Car {};
 	}
-
-	// std::exit(0);
-
-	// If sumocfg is not set in the command line, search for all files in
-	// cwd/**/*.sumocfg and print them
-	const int port = argv_parser.get<int>("port");
-	if (port < 0 || port > std::pow(2, 16) - 1) {
-		spdlog::error("Port must be between 0 and {}", std::pow(2, 16) - 1);
-		return 1;
-	}
-
-	const i32 simulation_steps = argv_parser.get<int>("simulation-steps");
-	if (simulation_steps <= 0) {
-		spdlog::error("simulation-steps must be positive");
-		return 1;
-	}
-
-	spdlog::info("simulation-steps: {}", simulation_steps);
-	spdlog::info("port: {}", port);
+	spdlog::info("Found {} vehicles in {}", cars.size(), sumocfg.route_files.string());
 
 	auto sumo_simulation_thread = std::thread([&]() -> void {
-		std::vector<std::string> cmd = {
-			argv_parser.get<bool>("gui") ? "sumo-gui" : "sumo",
-			"--configuration-file",
-			sumocfg_path.string(),
-		};
+		// std::vector<std::string> cmd = {
+		// 	argv_parser.get<bool>("gui") ? "sumo-gui" : "sumo",
+		// 	"--configuration-file",
+		// 	sumocfg_path.string(),
+		// };
 
 		// const auto simulation_start_result = Simulation::start(cmd, 19000);
 		// spdlog::info(".first: {} .second: {}", simulation_start_result.first,
 		// simulation_start_result.second);
 
 		const int num_retries = 100;
-		const int sumo_sim_port = 10000;
-		Simulation::init(sumo_sim_port, num_retries, "localhost");
+		// const int sumo_sim_port = 10000;
+		Simulation::init(options.sumo_port, num_retries, "localhost");
 		const double dt = Simulation::getDeltaT();
 
 		spdlog::info("dt: {}", dt);
 
 		// TODO: figure out how to subscribe an extract data from the simulation
-		for (int i = 0; i < simulation_steps; ++i) {
+		for (int i = 0; i < options.simulation_steps; ++i) {
 			Simulation::step();
 
 			// TODO: Get (x,y, theta) of all vehicles
@@ -376,11 +470,11 @@ auto main(int argc, char** argv) -> int {
 							counter++;
 						})
 
-				   .listen(port, [=](us_listen_socket_t* listen_socket) {
+				   .listen(options.port, [&](us_listen_socket_t* listen_socket) {
 					   if (listen_socket) {
-						   spdlog::info("Listening for connections on port {}\n", port);
+						   spdlog::info("Listening for connections on port {}\n", options.port);
 					   } else {
-						   spdlog::error("Failed to listen on port {}", port);
+						   spdlog::error("Failed to listen on port {}", options.port);
 					   }
 				   });
 
