b47a1a6ae10883b37f407df84c3cb679665e9a6f
kpbaks <kristoffer.pbs@tuta.io>
2023-12-14 17:30:52 +0100

    feat: add subscriber that puts light level information of streetlamps into sqlite3 db

---
+++ src/bin/streetlamps-subscriber.rs
@@ -0,0 +1,87 @@
+use std::collections::BTreeMap;
+use std::path::Path;
+
+use anyhow::Result;
+use log;
+use rusqlite;
+use serde::{Deserialize, Serialize};
+use serde_cbor;
+use zmq;
+
+
+// {
+//     timestamp: 1234567890,
+//     changes: {
+//         1: 0.5,
+//         2: 0.6,
+//         3: 0.7,
+//     }
+// }
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Message {
+    timestamp: i64,
+    changes: BTreeMap<i64, f32>,
+}
+
+
+fn main() -> Result<()> {
+    env_logger::init();
+
+    let context = zmq::Context::new();
+    let subscriber_socket = context
+        .socket(zmq::SUB)
+        .expect("Failed to create ZeroMQ SUB socket");
+    let host = "localhost";
+    let port: u16 = 12333;
+    let url = format!("tcp://{}:{}", host, port);
+    let topic = b"light_level";
+    subscriber_socket
+        .connect(&url)
+        .expect("Failed to connect to ZeroMQ SUB socket");
+    subscriber_socket
+        .set_subscribe(topic)
+        .expect("Failed to set subscription on ZeroMQ SUB socket");
+    log::info!("Connected to {}", url);
+
+    let db_file_path = Path::new("streetlamps.sqlite3");
+    if !db_file_path.exists() {
+        return Err(anyhow::anyhow!(
+            "Database file {} does not exist",
+            db_file_path.display()
+        ));
+    }
+    let mut connection = rusqlite::Connection::open(db_file_path)
+        .expect(format!("Failed to open {}", db_file_path.display()).as_str());
+
+    let query = "INSERT INTO measurements (streetlamp_id, timestamp, light_level) VALUES (:streetlamp_id, :timestamp, :light_level)";
+    // let insert_statement = connection
+    //     .prepare(query)
+    //     .expect("Failed to prepare SQL statement");
+
+    log::info!("Connected to {}", db_file_path.display());
+
+    loop {
+        let message = subscriber_socket.recv_bytes(0).unwrap();
+        let payload = &message[topic.len()..]; // Remove the topic from the message
+        let message: Message = serde_cbor::from_slice(payload).expect("Failed to decode CBOR message");
+        log::info!("Received message with timestamp {} and {} changes", message.timestamp, message.changes.len());
+
+        // TODO: figure out how to use a prepared statement here with transaction
+        let mut insert_statement = connection.prepare_cached(query)?;
+        // let mut transaction = connection.transaction()?;
+        connection.execute_batch("BEGIN")?;
+        for (streetlamp_id, light_level) in message.changes {
+            insert_statement.execute(
+                rusqlite::named_params! {
+                    ":streetlamp_id": streetlamp_id.to_string(),
+                    ":timestamp": message.timestamp,
+                    ":light_level": light_level,}
+            ).expect("Failed to execute SQL statement");
+        }
+        connection.execute_batch("COMMIT")?;
+        // transaction.commit()?;
+    }
+
+    Ok(())
+}
